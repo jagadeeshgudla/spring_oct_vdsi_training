df8141d810ad9eab8dace6e1c287d56724e1c682
Admin <Admin@Admin-PC>
2018-10-10 14:37:20 +0530

    Stream API changes added

--- FunctionalProgramming/src/com/demo/spring/StreamsExample.java
+++ FunctionalProgramming/src/com/demo/spring/StreamsExample.java
@@ -1,7 +1,17 @@
 package com.demo.spring;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class StreamsExample {
 	public static void main(String[] args) {
-		List<String> names = new ArrayList
+		List<String> names = new ArrayList<String>();
+		names.add("Jagadeesh");
+		names.add("Sarani");
+		names.add("Sriram");
+		
+		Stream<String> newNames= names.stream().filter((s)->s.startsWith("S"));
+		newNames.forEach((s)->System.out.println(s));
 	}
 }
