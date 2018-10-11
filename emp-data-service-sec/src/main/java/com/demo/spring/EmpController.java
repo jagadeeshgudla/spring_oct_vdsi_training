1609b8ac526628ba27eb6305a663613c7d7399c5
Admin <Admin@Admin-PC>
2018-10-11 15:48:21 +0530

    Spring Security

---
+++ emp-data-service-sec/src/main/java/com/demo/spring/EmpController.java
@@ -0,0 +1,72 @@
+package com.demo.spring;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.spring.entity.Emp;
+import com.demo.spring.repo.EmpRepository;
+
+@RestController
+public class EmpController {
+	@Autowired
+	EmpRepository empRepo;
+	
+	@RequestMapping(path="/info",method=RequestMethod.GET,produces="text/plain")
+	public String info() {
+		return "This is a sample REST Service";
+	}
+
+	@RequestMapping(path = "/emp/getEmp/{id}", method = RequestMethod.GET, produces = "application/json")
+	public Emp findEmpById(@PathVariable int id) {
+		System.out.println("Input::"+id);
+		Optional<Emp> emp = empRepo.findById(id);
+		if (emp.isPresent()) {
+			return emp.get();
+		} else {
+			throw new RuntimeException("No Data Found");
+		}
+	}
+
+	@RequestMapping(path = "/emp/getAllEmp", method = RequestMethod.GET, produces = "application/json")
+	public List<Emp> getAllEmp() {
+		List<Emp> empList = empRepo.findAll();
+		return empList;
+	}
+
+	@RequestMapping(path = "/emp/save", method = RequestMethod.POST, produces = "text/plain", consumes = "application/json")
+	public String addEmp(@RequestBody  Emp e) {
+		if(empRepo.existsById(e.getEmpId())) {
+			return "Emp Already Exists";
+		}else {
+			empRepo.save(e);
+		}
+		return "Created Successfully";
+	}
+	
+	
+	@RequestMapping(path = "/emp/update", method = RequestMethod.PUT, produces = "text/plain", consumes = "application/json")
+	public String updateEmp(@RequestBody Emp e) {
+		empRepo.save(e);
+		return "Emp Updated Successfully";
+	}
+	
+	
+	@RequestMapping(path = "/emp/delete", method = RequestMethod.DELETE, produces = "text/plain")
+	public String deleteEmp(@RequestParam  int id) {
+		if(empRepo.existsById(id)) {
+			empRepo.deleteById(id); 
+			return "Emp deleted";
+		}
+		
+		return "No Emp Found";
+	}
+
+}
--- spring-rest-client/src/main/java/com/demo/spring/GetClient.java
+++ spring-rest-client/src/main/java/com/demo/spring/GetClient.java
@@ -1,5 +1,7 @@
 package com.demo.spring;
 
+import org.apache.commons.codec.binary.Base64;
+
 import java.util.List;
 
 import org.springframework.core.ParameterizedTypeReference;
@@ -14,14 +16,20 @@ import com.demo.spring.entity.Emp;
 
 public class GetClient {
 	public static void main(String[] args) {
+		
+		String userDetails="pavan:gudla";
+		String encodedCred= new String(Base64.encodeBase64(userDetails.getBytes()));
+		System.out.println(encodedCred);
 		RestTemplate rt= new RestTemplate();
 		HttpHeaders headers= new HttpHeaders();
 		headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
 		headers.set("Accept", MediaType.TEXT_PLAIN_VALUE);
+		headers.set("Authorization", "Basic "+encodedCred);
 		Emp e = new Emp(2010,"Spring","US",2400);
 		HttpEntity req= new HttpEntity<>(e,headers);
-		ResponseEntity<String> resp=rt.exchange("http://localhost:8081/save", HttpMethod.POST, req,
+		ResponseEntity<String> resp=rt.exchange("http://localhost:8081/emp/save", HttpMethod.POST, req,
 				String.class);
+		//new ParameterizedTypeReference<List<Emp>>(){};
 		System.out.println("Response:"+resp.getBody());
  		System.out.println("Error Code:"+resp.getStatusCode());
 	}
